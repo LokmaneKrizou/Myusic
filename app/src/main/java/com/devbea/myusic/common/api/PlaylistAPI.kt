ca3917a2bbfe38b9eb32a30124e598d023556194
lokmanekrizou <krizou.lokmane@gmail.com>
2021-11-15 20:43:17 +0000

    reformat playlist api

--- app/src/main/java/com/devbea/myusic/common/api/PlaylistAPI.kt
+++ app/src/main/java/com/devbea/myusic/common/api/PlaylistAPI.kt
@@ -1,6 +1,5 @@
 package com.devbea.myusic.common.api
 
-import com.devbea.myusic.playlist.model.Playlist
 import com.devbea.myusic.playlist.model.PlaylistRaw
 import com.devbea.myusic.playlistDetails.model.PlaylistDetailsRaw
 import retrofit2.http.GET
@@ -8,9 +7,9 @@ import retrofit2.http.Path
 
 interface PlaylistAPI {
     @GET("/playlists")
-  suspend  fun fetchAllPlaylists(): List<PlaylistRaw>
+    suspend fun fetchAllPlaylists(): List<PlaylistRaw>
 
     @GET("/playlist/{id}")
-   suspend fun fetchPlaylistById(@Path("id")id: String):PlaylistDetailsRaw
+    suspend fun fetchPlaylistById(@Path("id") id: String): PlaylistDetailsRaw
 
 }
